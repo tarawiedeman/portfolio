b9d371686d3dcca7382d5a239d177c81f79afeef
tarawiedeman <33428501+tarawiedeman@users.noreply.github.com>
2023-06-05 15:02:07 -0700

    updated home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,8 +7,8 @@ function Home() {
   return (
 <section>
 <div className="home">
-    <img src={postcard} className="home__image"></img> 
-    <p className="home__name">Hi, Tara here. <br></br> <br></br>I'm a Full Stack Developer, travel-enthusiast, and dog-mom extraordinaire based out of beautiful Victoria, BC. <br></br><br></br> I recently graduated from BrainStation's full-stack software development diploma program, where I strengthened my skills in HTML, CSS, and Javascript. I am most comfortable working with React JS as a front-end framework and NodeJS and ExpressJS as back-end frameworks but have a breadth of other knowledge and a curious mind! <br></br><br></br> I've worked in tech for seven years in Technical Solutions Consulting and have a degree in Business in Science so I'm equal parts logical problem-solver and relationship builder. I love problem-solving and designing software solutions that make customers happy! <br></br><br></br>I'm currently searching for a position as a Junior Software Developer. </p>
+    <img src={postcard} className="home__image" alt="postcard"></img> 
+    <p className="home__name">Hi, Tara here. <br></br> <br></br>I'm a Full Stack Developer, travel-enthusiast, and dog-mom extraordinaire based out of beautiful Victoria, BC. <br></br><br></br> I recently graduated from BrainStation's full-stack software development diploma program, where I strengthened my skills in HTML, Javascript, TypeScript, React, SQL, Node, Express and many others. I am most comfortable working with React JS as a front-end framework and NodeJS and ExpressJS as back-end frameworks but have a breadth of other knowledge and a curious mind! <br></br><br></br> I've worked in tech for seven years in Technical Solutions Consulting and have a degree in Business in Science so I'm equal parts logical problem-solver and relationship builder. I love problem-solving and designing software solutions that make customers happy! <br></br><br></br>I'm currently searching for a position as a Junior Software Developer. </p>
 </div>
 
 <div className="home__social">
